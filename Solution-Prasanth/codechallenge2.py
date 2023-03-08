579b2436cb421b5e864c78e6c24cd10a54f80c95
Prasanth P <3108prasanthp@gmail.com>
2023-03-08 10:20:26 +0530

    Python Solution Files by Prasanth P

---
+++ Solution-Prasanth/codechallenge2.py
@@ -0,0 +1,71 @@
+import pandas as pd
+df = pd.read_csv("C:/Users/3108p/code_challenge/vendor-capacities.csv")
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+purchases = []
+result = []
+
+@app.route("/purchases", methods=["GET"])
+def get_purchase():
+    return jsonify(purchases)
+
+@app.route("/purchases", methods=["POST"])
+def computing_result():
+    result_dictionary={}
+    if request.is_json:
+        purchase = request.get_json()
+        purchases.append(purchase)
+        mincost = 100000
+        mintime = 1000
+        isavail = "false"
+        vendor = " "
+        for j in range(df.shape[0]):
+            
+            if purchase["item"] == df["Item"][j]:
+                
+                if int(df["Default Quantity Limit in KG"][j]) >= int(purchase["Req. Quantity"]):
+                    
+                    cost = int(df["Cost Per KG"][j])*int(purchase["Req. Quantity"])
+                
+                else:
+                
+                    additional = int(df["Cost Per KG"][j]) + int(df["Additional Cost Per KG"][j])
+                    addtocost = ((int(purchase["Req. Quantity"])-int(df["Default Quantity Limit in KG"][j])) * additional)
+                    cost = (int(df["Cost Per KG"][j]) * int(df["Default Quantity Limit in KG"][j])) + addtocost
+            
+                if cost < mincost: 
+                    mincost = int(cost)
+                    mintime = int(df["Time to Deliver"][j])
+                    isavail = "true"
+                    vendor = df["Vendor"][j]
+                    
+                elif cost == mincost:
+                    
+                    if df["Time to Deliver"][j] < mintime:
+                        
+                        mintime = int(df["Time to Deliver"][j])
+                        isavail = "true"
+                        vendor = df["Vendor"][j]
+                        
+        result_dictionary["Purchase Id"] = purchase["Pur_id"]
+        result_dictionary["Item"] = purchase["item"]
+        result_dictionary["Quantity"] = purchase["Req. Quantity"]
+        result_dictionary["IsAvail"] = isavail
+        result_dictionary["Vendor"] = vendor
+        if mincost == 100000 and mintime == 1000:
+            total=0
+            time=0
+        else:
+            total=mincost
+            time=mintime
+        result_dictionary["Total cost"] = total
+        result_dictionary["Time"] = time
+        result.append(result_dictionary)
+        return result_dictionary,201
+    return {"error": "Request must be JSON"}, 415
+
+@app.route("/results", methods=["GET"])
+def get_result():
+    return jsonify(result)
